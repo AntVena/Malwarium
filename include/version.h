df29ce7c4b14c86d0246cfa182b4bc17b1a446b5
Joshua Fembock <josh_writes_code@pm.me>
2026-08-27 08:09:22 +1000

    release v0.37.0: hits land where the stage has room, and a malbeast fights like itself

--- include/version.h
+++ include/version.h
@@ -41,7 +41,7 @@
 #  define FW_VERSION_MAJOR 0
 #endif
 #ifndef FW_VERSION_MINOR
-#  define FW_VERSION_MINOR 36
+#  define FW_VERSION_MINOR 37
 #endif
 #ifndef FW_VERSION_PATCH
 #  define FW_VERSION_PATCH 0
